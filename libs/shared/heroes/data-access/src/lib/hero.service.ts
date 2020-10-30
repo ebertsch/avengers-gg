5f366b54f6c39d7d60bb48371aec1a8c3f94516b
Ezra Bertsch <ebertsch@gmail.com>
2020-10-30 12:47:59 -0400

    reworked observables to function properly

--- libs/shared/heroes/data-access/src/lib/hero.service.ts
+++ libs/shared/heroes/data-access/src/lib/hero.service.ts
@@ -7,6 +7,7 @@ import { select, createSelector } from '@ngrx/store';
 import { RouteSelectors } from '@avengers-game-guide/shared/router'
 
 import { Hero } from './hero';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 
 @Injectable({
