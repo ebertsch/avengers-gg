ecf421b1be0074c835b39536cdcd7d0f0a978e66
Ezra Bertsch <ebertsch@gmail.com>
2020-10-31 16:38:37 -0400

    Clear cache on view load

--- libs/heroes/heroes-detail/src/lib/notes-view/notes-view.component.ts
+++ libs/heroes/heroes-detail/src/lib/notes-view/notes-view.component.ts
@@ -19,7 +19,7 @@ export class NotesViewComponent implements OnInit {
   constructor(private notesService: NoteService, private heroes: HeroService, private titleService: Title) {
     this.notes$ = this.heroes.selected$.pipe(
       tap(hero => this.titleService.setTitle(`Avengers GG | Notes | ${hero.name}`)),
-      tap(hero => this.notesService.getWithQuery({heroId: hero.id})),
+      tap(hero => { this.notesService.clearCache(); this.notesService.getWithQuery({heroId: hero.id})}),
       switchMap(() => this.notesService.entities$),
       map(notes => groupBy(prop('category'), notes))
     )
