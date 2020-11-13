beaf6fac63708802272f6faa4839827bd205b4ff
Ezra Bertsch <ebertsch@gmail.com>
2020-11-13 13:09:52 -0500

    Ironman data validation

--- apps/data-entry/src/app/pages/perks-page/perks-page.component.ts
+++ apps/data-entry/src/app/pages/perks-page/perks-page.component.ts
@@ -3,6 +3,8 @@ import { StorageMap } from '@ngx-pwa/local-storage';
 import { HeroService } from '@avengers-game-guide/shared/heroes/data-access';
 import { Perk, PerkService } from '@avengers-game-guide/shared/perks/data-access';
 import { BaseDataPage } from '../base-data-page'
+import { GearDefinition, GearService } from '@avengers-game-guide/shared/gear/data-access';
+import { BehaviorSubject } from 'rxjs';
 
 
 @Component({
@@ -13,8 +15,11 @@ import { BaseDataPage } from '../base-data-page'
 })
 export class PerksPageComponent extends BaseDataPage<Perk> implements OnInit {
   FILTER_KEY = 'filters:perks';
+
+  private gear = new BehaviorSubject<GearDefinition[]>([]);
+
   
-  constructor(storage: StorageMap, public perkService: PerkService, public heroService: HeroService) {
+  constructor(storage: StorageMap, public perkService: PerkService, public heroService: HeroService, public gearService: GearService) {
     super(storage, perkService)
   }
 
