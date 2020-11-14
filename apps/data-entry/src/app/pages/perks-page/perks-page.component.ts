db73034df82d77a13c3dbbd4122a563d19275874
Ezra Bertsch <ebertsch@gmail.com>
2020-11-14 16:09:47 -0500

    UI Updates

--- apps/data-entry/src/app/pages/perks-page/perks-page.component.ts
+++ apps/data-entry/src/app/pages/perks-page/perks-page.component.ts
@@ -3,9 +3,9 @@ import { StorageMap } from '@ngx-pwa/local-storage';
 import { HeroService } from '@avengers-game-guide/shared/heroes/data-access';
 import { Perk, PerkService } from '@avengers-game-guide/shared/perks/data-access';
 import { BaseDataPage } from '../base-data-page'
-import { GearDefinition, GearService } from '@avengers-game-guide/shared/gear/data-access';
-import { takeWhile, tap, map } from 'rxjs/operators';
-import { flatten, map as rmap, pick, reduce, values } from 'ramda';
+import { GearService } from '@avengers-game-guide/shared/gear/data-access';
+import { takeWhile, map } from 'rxjs/operators';
+import { flatten, map as rmap, pick, values } from 'ramda';
 
 
 @Component({
@@ -17,26 +17,28 @@ import { flatten, map as rmap, pick, reduce, values } from 'ramda';
 export class PerksPageComponent extends BaseDataPage<Perk> implements OnInit {
   FILTER_KEY = 'filters:perks';
 
-  gear:string[][];
-  
+  gear: string[][];
+
   constructor(storage: StorageMap, public perkService: PerkService, public heroService: HeroService, public gearService: GearService) {
     super(storage, perkService)
 
+
     this.gearService.entities$.pipe(
       takeWhile(() => this.gear !== null),
-      map(items => 
+      map(items =>
         rmap(i => flatten(values(pick(['perks1', 'perks2', 'perks3'], i))) as string[], items)
       )
     ).subscribe(x => this.gear = x);
   }
 
-  appearsOnGear(perk: Perk) {
-    if((this.gear||[]).length < 1) return 0;
-
-    return reduce((acc, cur)=>{
-      const isUsed = cur.indexOf(perk.id) > -1  ? 1 : 0
-      return acc + isUsed
-    }, 0, this.gear)
+  ngOnInit() {
+    super.ngOnInit()
+    // this.filteredItems$ = this.primaryDataService.filteredEntities$.pipe(
+    //   withLatestFrom(this.gearService.entities$),
+    //   map(([items, gear]) => {
+    //     return items.filter(i => !i.slot1Enabled && !i.slot2Enabled && !i.slot3Enabled)
+    //   })
+    // )
   }
 
 }
