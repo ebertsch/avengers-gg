fc3c64825fdeaad811e22ae412d7268ff8eae8ce
Ezra Bertsch <ebertsch@gmail.com>
2020-11-17 07:11:44 -0500

    Normalize buttons

--- libs/shared/gear/loadout-editor/src/lib/shared-gear-loadout-editor.module.ts
+++ libs/shared/gear/loadout-editor/src/lib/shared-gear-loadout-editor.module.ts
@@ -10,10 +10,11 @@ import { LoadoutSummaryComponent } from './loadout-summary/loadout-summary.compo
 import { RouterModule } from '@angular/router';
 import { MatSelectModule } from '@angular/material/select'
 import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 import { SharedGearGearViewerModule } from '@avengers-game-guide/shared/gear/gear-viewer'
 
 @NgModule({
-  imports: [CommonModule, SharedUiModule, FormsModule, ReactiveFormsModule, RouterModule, MatSelectModule, MatButtonModule, SharedGearGearViewerModule],
+  imports: [CommonModule, SharedUiModule, FormsModule, ReactiveFormsModule, RouterModule, MatSelectModule, MatButtonModule, MatIconModule, SharedGearGearViewerModule],
   declarations: [LoadoutBuilderComponent, GearInstanceEditorComponent, GearInstanceViewerComponent, LoadoutSummaryComponent],
   exports: [LoadoutBuilderComponent],
 })
